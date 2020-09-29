6787dced918bd2d919a387eb5663911d3eb8331b
randi fjeld <rfjeld@student.42.fr>
2020-09-29 18:26:03 +0200

    finally?

---
+++ c04/ft_putnbr_base.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfjeld <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/20 20:34:06 by rfjeld            #+#    #+#             */
+/*   Updated: 2020/09/28 19:11:09 by rfjeld           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_char(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb, int count, char *base)
+{
+	long int n;
+
+	n = nb;
+	if (n < 0)
+	{
+		ft_print_char('-');
+		n = -n;
+	}
+	if (n >= count)
+	{
+		ft_putnbr((n / count), count, base);
+		ft_print_char(base[n % count]);
+	}
+	else
+		ft_print_char(base[n % count]);
+}
+
+int		ft_check_base(char *base)
+{
+	int i;
+	int count;
+	int j;
+
+	count = 0;
+	i = 0;
+	while (base[i])
+	{
+		count = count + 1;
+		if (base[i] == '-' || base[i] == '+')
+			return (0);
+		j = i - 1;
+		while (j > 0)
+		{
+			if (base[i] == base[j])
+				return (0);
+			--j;
+		}
+		i++;
+	}
+	if (count <= 1)
+		return (0);
+	return (count);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int count;
+
+	count = (ft_check_base(base));
+	if (count == 0)
+		return ;
+	ft_putnbr(nbr, count, base);
+}
